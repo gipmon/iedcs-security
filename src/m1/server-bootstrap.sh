24ad4740409e1f2a7d40cf0a49238ef0ab301e7d
Rafael Ferreira <mail@rafaelferreira.pt>
2015-10-11 10:05:23 +0100

    install option

--- src/m1/server-bootstrap.sh
+++ src/m1/server-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install python python-pip python-dev
+apt-get install -y python python-pip python-dev
 cd /vagrant/iedcs-server
 pip install -r requirements.txt
