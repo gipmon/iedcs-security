0e4f54e300c32125a7ee7d8c3b7a2b0df95bd471
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-10-27 14:09:32 +0000

    restriction

--- src/m1/iedcs-server/restrictions/views.py
+++ src/m1/iedcs-server/restrictions/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import get_object_or_404
 from django.db import IntegrityError
 from rest_framework.response import Response
-from .models import Book, OrderBook
-from .serializers import BookSerializer, MakeOrderSerializer
-from .renders import PlainTextRenderer
+from .models import Book, BookRestrictions
+from books.models import OrderBook
+from .serializers import BookRestrictionsSerializer
 from rest_framework import permissions
 from rest_framework import viewsets, status, mixins, views
+from .restrictions import test_restriction
 from django.db import transaction
+import json
 
 
-class OrderViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
-    queryset = OrderBook.objects.filter()
-    serializer_class = BookSerializer
+class BooksRestrictionsViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = BookRestrictions.objects.filter()
+    serializer_class = BookRestrictionsSerializer
 
     def get_permissions(self):
         return permissions.IsAuthenticated(),
@@ -24,29 +26,25 @@ class OrderViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.Gene
         :type  books_identifier: str
         :param books_identifier: the books that the user wants to read
         """
-        serializer = MakeOrderSerializer(data=request.data)
+        serializer = BookRestrictionsSerializer(data=request.data)
 
         if serializer.is_valid():
-            try:
-                user = request.user
-                # verify if the book exists
-                book = get_object_or_404(Book.objects.all(), identifier=serializer.validated_data['book_identifier'])
+            user = request.user
+            # verify if the book exists
+            book = get_object_or_404(Book.objects.all(), identifier=serializer.validated_data['book_identifier'])
 
-                # verify if the user already purchase the book
-                if OrderBook.objects.filter(buyer=user, book=book).count() == 1:
-                    return Response({'status': 'Bad Request',
-                                     'message': 'You already purchase the book: ' + book.name},
-                                    status=status.HTTP_400_BAD_REQUEST)
-
-                with transaction.atomic():
-                    # new order
-                    OrderBook.objects.create(buyer=user, book=book)
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-            except IntegrityError:
-                return Response({'status': 'Bad request',
-                                 'message': 'The book couldn\'t be purchase!'},
+            # verify if the user already purchase the book
+            if OrderBook.objects.filter(buyer=user, book=book).count() != 1:
+                return Response({'status': 'Bad Request',
+                                 'message': 'You don\'t have purchased the book: ' + book.name},
                                 status=status.HTTP_400_BAD_REQUEST)
+
+            can = True
+
+            for book_restriction in BookRestrictions.objects.filter(book=book):
+                can &= test_restriction(book_restriction.restriction.restrictionFunction, book, user.user_data)
+
+            return Response(json.dumps(["restricted": ), status=status.HTTP_201_CREATED)
 
         return Response({'status': 'Bad Request',
                          'message': serializer.errors},
