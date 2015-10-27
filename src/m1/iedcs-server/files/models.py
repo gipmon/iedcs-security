800f5c166e66dc2d6478a71910371a8fa2459a56
Rafael Ferreira <mail@rafaelferreira.pt>
2015-10-27 22:06:20 +0000

    new models

--- src/m1/iedcs-server/files/models.py
+++ src/m1/iedcs-server/files/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-import uuid
-
 from authentication.models import Account
 from books.models import Book
 
 
 class File(models.Model):
-    identifier = models.CharField(max_length=128, default=uuid.uuid4, blank=False, unique=True)
-    path = models.FileField()
     book = models.ForeignKey(Book, blank=False)
     buyer = models.ForeignKey(Account, blank=False)
 
---
+++ src/m1/iedcs-server/players/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from authentication.models import Account
+
+
+class Device(models.Model):
+    unique_identifier = models.CharField(max_length=254, default="", unique=True)
+
+    owner = models.ForeignKey(Account)
+
+    cpu_model = models.CharField(max_length=128, default="")
+    op_system = models.CharField(max_length=128, default="")
+    ip = models.CharField(max_length=128, default="")
+    country = models.CharField(max_length=128, default="")
+    timezone = models.CharField(max_length=128, default="")
+
+    public_key = models.FileField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Player(models.Model):
+    version = models.CharField(max_length=128, default="", unique=True)
+    public_key = models.FileField()
+    private_key = models.FileField()
