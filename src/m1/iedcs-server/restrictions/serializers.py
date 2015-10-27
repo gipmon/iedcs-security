c44abb8b16e729b52d2a72f0809155505f86c5e4
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-10-27 14:10:16 +0000

    retrictions

---
+++ src/m1/iedcs-server/restrictions/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Restriction, BookRestrictions
+
+class BookRestrictionsSerializer(serializers.ModelSerializer):
+    book_identifier = serializers.CharField(max_length=128)
+
+    class Meta:
+        model = BookRestrictions
+        fields = ('book_identifier',)
+        read_only_fields = ('book_identifier',)
+
