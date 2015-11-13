14b4fed30e97df87577d7dde50e514449f15afd8
Rafael Ferreira <rafael.ferreira@ua.pt>
2015-11-13 10:59:02 +0000

    server bootstrap withtout datbase, creates one

--- src/m1/installation/server-bootstrap.sh
+++ src/m1/installation/server-bootstrap.sh
@@ -4,6 +4,13 @@ apt-get update
 apt-get install -y python python-pip python-dev apache2 libapache2-mod-wsgi
 cd /var/www/
 pip install -r requirements.txt
+
+# database
+if [ ! -f db.sqlite3 ]; then
+  python manage.py migrate
+  python manage.py insert_books
+fi
+
 cp /vagrant/iedcs.rafaelferreira.pt.conf /etc/apache2/sites-available/iedcs.rafaelferreira.pt.conf
 a2ensite iedcs.rafaelferreira.pt.conf
 sudo a2enmod ssl
