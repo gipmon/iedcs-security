0240a701594ced603532a9cbc8e220de04a2cd20
Rafael Ferreira <rafael.ferreira@ua.pt>
2015-12-11 23:54:07 +0000

    need pteid

--- src/installation/install_cert.sh
+++ src/installation/install_cert.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rm -rf cacerts.keystore
-keytool -import -alias alias -keystore cacerts.keystore -file CA/iedcs.rafaelferreira.pt.crt
+keytool -import -alias iedcs -keystore cacerts.keystore -file CA/iedcs.rafaelferreira.pt.crt
+keytool -import -alias player -keystore cacerts.keystore -file cacert.pem
