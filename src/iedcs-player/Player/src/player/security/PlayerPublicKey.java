90b43ee9a8e19dfae562726628588c8ef360ed88
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-12-26 16:57:46 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	report/relatorio.pdf
    #	report/relatorio.tex (reverted from commit 8b58f1748e5613551bc3f6ea63384d68845f6245)

--- src/iedcs-player/Player/src/player/security/PlayerPublicKey.java
+++ src/iedcs-player/Player/src/player/security/PlayerPublicKey.java
@@ -10,7 +10,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,17 +17,36 @@ import java.util.logging.Logger;
 public class PlayerPublicKey {
     
     public static PublicKey getKey(){
+        FileInputStream fis = null;
+        
         try {
-            String key_b64 = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqZ8rdPCND6AVoCqHmDWYZqG9X4oukF31SpU0pjYb4p70eS4J+mXKd2Y2S/fnM0IguxAbTWEtefS8tLztxrn1ign8tWo7EwuRmeb5vnof6/14Nwn+wmkx5Zq/Uwyay8DwwGtClg5zQfLHaRQ4IBH5uxCTMl33jq8oBOSMKL/xHIkynSZwWegyAgEEkhyadx3PnOfmBLo7X+/MrLE/5Ec1d8taFVgFYynw7VWJfCITN/nioDxQCQXv7n6E29MLjLyaHwopWSjiOv4PKFP3xCZlFuq0nF9Z58foU1ngkqPTugOLzhhSOK35zgyZJ4xx/GAwb7rtxGitcHyXTB0WcFRnjwIDAQAB";
+            File f = new File("keystore/player.pub");
             
-            byte[] keyBytes = Base64.getDecoder().decode(key_b64.getBytes());
+            if(f.exists()){
+                System.out.println("AQUI");
+            }
+            
+            fis = new FileInputStream(f);
+            DataInputStream dis = new DataInputStream(fis);
+            
+            byte[] keyBytes = new byte[(int)f.length()];
+            dis.readFully(keyBytes);
+            dis.close();
             
             X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
             KeyFactory kf = KeyFactory.getInstance("RSA");
             
             return kf.generatePublic(spec);
-        } catch (InvalidKeySpecException | NoSuchAlgorithmException ex) {
+        } catch (FileNotFoundException ex) {
             Logger.getLogger(PlayerPublicKey.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException | IOException ex) {
+            Logger.getLogger(PlayerPublicKey.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException ex) {
+                Logger.getLogger(PlayerPublicKey.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
         return null;
